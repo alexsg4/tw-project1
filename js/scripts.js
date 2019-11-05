b2578ab88c403910a39b09205936e9afb1ea3bc9
Alexandru Stanciu <stanciu.alex74@gmail.com>
2019-11-06 00:55:00 +0200

    Toggle dropdown menu on click

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ function toggleMobileNavbar() {
     x.className = "navbar";
   }
 }
+
+function toggleDropdown(elem) {
+  var dropdownContent = elem.getElementsByClassName("dropdown-content")[0];
+  var displayType = dropdownContent.style.display;
+  if(displayType === "none"){
+    dropdownContent.style.display = "block";
+  } else {
+    dropdownContent.style.display = "none";
+  }
+}
